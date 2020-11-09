da0945bdf02660a263d9e979de458e821164a62f
supanidaa <31795405+supanidaa@users.noreply.github.com>
2020-11-09 12:21:17 +0700

    profilepage fix

--- newlaundry/lib/userpage/profilepage.dart
+++ newlaundry/lib/userpage/profilepage.dart
@@ -1,9 +1,11 @@
-// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:newlaundry/laundrypage/profilelaundry.dart';
 import 'package:newlaundry/signinpage/signinpage.dart';
 import 'package:newlaundry/userpage/profileuser/accountpage.dart';
-import 'package:newlaundry/userpage/profileuser/addresspage.dart';
+// import 'package:newlaundry/userpage/profileuser/addresspage.dart';
+// import 'package:newlaundry/widgets/google_signin.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -11,6 +13,30 @@ class ProfilePage extends StatefulWidget {
 }
 
 class ProfilePageState extends State<ProfilePage> {
+  String login;
+
+  @override
+  void initState() {
+    super.initState();
+    findDisplay();
+  }
+
+  Future<void> findDisplay() async {
+    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    // FirebaseUser firebaseUser = await firebaseAuth.currentUser();
+    // setState(() {
+    //   login = firebaseUser.displayName;
+    // });
+    // print('login = $login');
+
+    if (firebaseAuth.currentUser != null) {
+      setState(() {
+        login = firebaseAuth.currentUser.email;
+      });
+      print(firebaseAuth.currentUser.email);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -65,12 +91,12 @@ class ProfilePageState extends State<ProfilePage> {
                   Align(
                     alignment: Alignment.center,
                     child: CircleAvatar(
-                      radius: 70,
+                      radius: 30,
                       backgroundColor: Colors.white,
                       child: ClipOval(
                         child: SizedBox(
-                          width: 180,
-                          height: 180,
+                          // width: 100,
+                          // height: 100,
                           child: Image.asset(
                             'assets/boy.png',
                             fit: BoxFit.fill,
@@ -84,16 +110,17 @@ class ProfilePageState extends State<ProfilePage> {
               Column(
                 children: [
                   Container(
-                    padding: EdgeInsets.only(left: 20),
+                    padding: EdgeInsets.only(left: 10),
                     child: Text(
-                      'widget.user.displayNamed',
+                      '$login',
                       style: TextStyle(
                           color: Colors.black,
                           fontFamily: 'Prompt',
                           fontSize: 16,
                           fontWeight: FontWeight.w300),
+                      overflow: TextOverflow.fade,
                     ),
-                  )
+                  ),
                 ],
               )
             ],
@@ -144,52 +171,52 @@ class ProfilePageState extends State<ProfilePage> {
               ),
             ),
           ),
-          Container(
-            margin: EdgeInsets.only(left: 10, right: 10),
-            width: double.maxFinite,
-            child: InkWell(
-              onTap: () => {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AddressPage()),
-                ),
-              },
-              child: Card(
-                child: Container(
-                  height: 80,
-                  padding: EdgeInsets.only(left: 20),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.location_on,
-                        color: Colors.blue,
-                        size: 25,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(right: 20),
-                      ),
-                      Text(
-                        "ที่อยู่ของฉัน ",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontFamily: 'Prompt',
-                            fontSize: 16,
-                            fontWeight: FontWeight.w300),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(90),
-                      ),
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        size: 25,
-                        color: Colors.grey,
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
+          // Container(
+          //   margin: EdgeInsets.only(left: 10, right: 10),
+          //   width: double.maxFinite,
+          //   child: InkWell(
+          //     onTap: () => {
+          //       Navigator.push(
+          //         context,
+          //         MaterialPageRoute(builder: (context) => AddressPage()),
+          //       ),
+          //     },
+          //     child: Card(
+          //       child: Container(
+          //         height: 80,
+          //         padding: EdgeInsets.only(left: 20),
+          //         child: Row(
+          //           children: <Widget>[
+          //             Icon(
+          //               Icons.location_on,
+          //               color: Colors.blue,
+          //               size: 25,
+          //             ),
+          //             Padding(
+          //               padding: EdgeInsets.only(right: 20),
+          //             ),
+          //             Text(
+          //               "ที่อยู่ของฉัน ",
+          //               style: TextStyle(
+          //                   color: Colors.black,
+          //                   fontFamily: 'Prompt',
+          //                   fontSize: 16,
+          //                   fontWeight: FontWeight.w300),
+          //             ),
+          //             Padding(
+          //               padding: EdgeInsets.all(90),
+          //             ),
+          //             Icon(
+          //               Icons.arrow_forward_ios,
+          //               size: 25,
+          //               color: Colors.grey,
+          //             )
+          //           ],
+          //         ),
+          //       ),
+          //     ),
+          //   ),
+          // ),
           SizedBox(height: 50),
           Container(
             margin: EdgeInsets.only(left: 100, right: 100),
