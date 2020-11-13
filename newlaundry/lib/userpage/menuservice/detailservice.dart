b248419a520f1770bd23fff5e43f5a6054565b80
supanidaa <31795405+supanidaa@users.noreply.github.com>
2020-11-13 19:38:23 +0700

    fix detail

--- newlaundry/lib/userpage/menuservice/detailservice.dart
+++ newlaundry/lib/userpage/menuservice/detailservice.dart
@@ -55,7 +55,7 @@ class DetailServiceState extends State<DetailServicePage> {
                       ),
                       child: Container(
                         margin: EdgeInsets.all(5),
-                        child: Image.asset('assets/polo-shirt.png'),
+                        child: Image.asset('assets/laundry-basket.png'),
                       ),
                     ),
                     Container(
